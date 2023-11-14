8e92e6541ee045ad698b0500d2703e6d1525e195
solomon ashish bolleddu <68480526+solomonbolleddu@users.noreply.github.com>
2023-11-14 19:56:10 +0530

    created file selpy
    1st changes made

---
+++ selpy.py
@@ -0,0 +1,15 @@
+import time
+from selenium import webdriver
+driver = webdriver.Chrome("C:/Users/sbolleddu/Downloads/chromedriver_win32/chromedriver.exe")
+driver.maximize_window()
+driver.get('https://www.amazon.in/')
+listA = driver.find_elements_by_xpath("//a")
+tempo = 0
+for each in listA:
+    tempo = tempo+1
+
+    if each.text == "Amazon Pay":
+        time.sleep(5)
+        driver.find_element_by_partial_link_text(each.text).click()
+        break
+print(tempo)
